a95c8f114cd250741451f679b7a7bb992f3c1531
firehook <diginital@bigmir.net>
2019-02-01 20:48:19 +0200

    Added google map to activity.

--- app/src/main/java/com/example/vladyslav/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/vladyslav/weatherforecast/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.vladyslav.weatherforecast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+
     }
 }
